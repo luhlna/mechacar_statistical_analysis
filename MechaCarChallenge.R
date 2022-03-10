668981db593d032e83793855f81467dd00fdb2cd
luhlna <heli.dehesa@gmail.com>
2022-03-10 05:19:05 -0600

    T-Test on Suspension Coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,3 +14,15 @@ total_summary
 
 lot_summary <- suspensionCoilData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
 lot_summary
+
+t_test <- t.test(suspensionCoilData$PSI, mu= 1500)
+t_test
+
+t_test_1 <- t.test(subset(suspensionCoilData,Manufacturing_Lot =="Lot1")$PSI, mu =1500)
+t_test_1
+
+t_test_2 <- t.test(subset(suspensionCoilData,Manufacturing_Lot =="Lot2")$PSI, mu =1500)
+t_test_2
+
+t_test_3 <- t.test(subset(suspensionCoilData,Manufacturing_Lot =="Lot3")$PSI, mu =1500)
+t_test_3
